4f630fc383433cc17288798081992fd4a7e5df89
Javier-DlaP <javirachi99@hotmail.es>
2021-03-16 13:17:33 +0100

    Add direction to detections

--- display_kitti_pcl/display_pcl.py
+++ display_kitti_pcl/display_pcl.py
@@ -57,24 +57,12 @@ def addBoundingBox(object3d):
        /|         /|
       1 -------- 3 .
       | |        | |
-      . 4 -------- 6
-      |/         |/
+      . 4 ----8--- 6
+      |/     9   |/
       0 -------- 2
-    """
-    """
-    p0 = [object3d.tx, object3d.ty, object3d.tz]
-    p1 = [object3d.tx, object3d.ty, object3d.tz+object3d.h]
-    p2 = [object3d.tx-object3d.w*math.sin(object3d.rz), object3d.ty+object3d.w*math.cos(object3d.rz), object3d.tz]
-    p3 = [object3d.tx-object3d.w*math.sin(object3d.rz), object3d.ty+object3d.w*math.cos(object3d.rz), object3d.tz+object3d.h]
-    p4 = [object3d.tx+object3d.l*math.cos(object3d.rz), object3d.ty+object3d.l*math.sin(object3d.rz), object3d.tz]
-    p5 = [object3d.tx+object3d.l*math.cos(object3d.rz), object3d.ty+object3d.l*math.sin(object3d.rz), object3d.tz+object3d.h]
-    p6 = [object3d.tx+object3d.l*math.cos(object3d.rz)-object3d.w*math.sin(object3d.rz), object3d.ty+object3d.w*math.cos(object3d.rz)+object3d.l*math.sin(object3d.rz), object3d.tz]
-    p7 = [object3d.tx+object3d.l*math.cos(object3d.rz)-object3d.w*math.sin(object3d.rz), object3d.ty+object3d.w*math.cos(object3d.rz)+object3d.l*math.sin(object3d.rz), object3d.tz+object3d.h]
     """
     tx = object3d.tx+object3d.w/2*math.sin(object3d.rz)-object3d.l/2*math.cos(object3d.rz)
     ty = object3d.ty-object3d.w/2*math.cos(object3d.rz)-object3d.l/2*math.sin(object3d.rz)
-    #tx = object3d.tx
-    #ty = object3d.ty
 
     p0 = [tx, ty, object3d.tz]
     p1 = [tx, ty, object3d.tz+object3d.h]
@@ -84,12 +72,15 @@ def addBoundingBox(object3d):
     p5 = [tx+object3d.l*math.cos(object3d.rz), ty+object3d.l*math.sin(object3d.rz), object3d.tz+object3d.h]
     p6 = [tx+object3d.l*math.cos(object3d.rz)-object3d.w*math.sin(object3d.rz), ty+object3d.w*math.cos(object3d.rz)+object3d.l*math.sin(object3d.rz), object3d.tz]
     p7 = [tx+object3d.l*math.cos(object3d.rz)-object3d.w*math.sin(object3d.rz), ty+object3d.w*math.cos(object3d.rz)+object3d.l*math.sin(object3d.rz), object3d.tz+object3d.h]
+    p8 = [(p4[0]+p6[0])/2,(p4[1]+p6[1])/2,object3d.tz]
+    p9 = [(p0[0]+p2[0]+p4[0]+p6[0])/4,(p0[1]+p2[1]+p4[1]+p6[1])/4,object3d.tz]
+
 
     x = []
     y = []
     z = []
 
-    for elem in [p0, p1, p3, p2, p0, p4, p5, p7, p6, p4, p0, p6, p2, p4, p1, p5, p0, p3, p1, p2, p7, p3, p6, p5, p4, p7, p1, p3, p5]:
+    for elem in [p0, p2, p3, p1, p0, p4, p5, p1, p3, p7, p6, p2, p6, p8, p9, p8, p4, p5, p7]:
         x.append(elem[0])
         y.append(elem[1])
         z.append(elem[2])
